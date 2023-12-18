a6a8780a00babf0184dd8d029fd8eaab3fb7e354
Exzq_ <pariwat_wong@cmu.ac.th>
2023-12-18 14:00:37 +0700

    hope so

--- src/Lab07.java
+++ src/Lab07.java
@@ -1,11 +1,15 @@
 import java.util.*;
 
+// Initializing each type & method
 interface LabSet<E> {
     boolean add(E e);
     boolean contains(E e);
     boolean remove(E e);
     int size();
-    Object[] toArray();
+    boolean containsAll(Collection<?> c);
+    boolean addAll(Collection<? extends E> c);
+    boolean removeAll(Collection<?> c);
+    boolean retainAll(Collection<?> c);
 }
 
 class HashMapSet<E> implements LabSet<E> {
@@ -15,6 +19,7 @@ class HashMapSet<E> implements LabSet<E> {
         this.map = new HashMap<>();
     }
 
+    // Adds the specified element to this set if it is not already present
     @Override
     public boolean add(E e) {
         if (!contains(e)) {
@@ -24,11 +29,13 @@ class HashMapSet<E> implements LabSet<E> {
         return false;
     }
 
+    // Returns true if this set contains the specified element
     @Override
     public boolean contains(E e) {
         return map.containsKey(e);
     }
 
+    // Removes the specified element from this set if it is present
     @Override
     public boolean remove(E e) {
         if (contains(e)) {
@@ -38,16 +45,36 @@ class HashMapSet<E> implements LabSet<E> {
         return false;
     }
 
+    // Returns the number of elements in this set
     @Override
     public int size() {
         return map.size();
     }
 
+    // New methods
     @Override
-    public Object[] toArray(){
-        return map.keySet().toArray();
+    public boolean containsAll(Collection<?> c) {
+        return map.keySet().containsAll(c);
     }
 
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        boolean modified = false;
+        for (E element : c) {
+            modified |= add(element);
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return map.keySet().removeAll(c);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return map.keySet().retainAll(c);
+    }
 }
 
 public class Lab07 {
@@ -56,22 +83,41 @@ public class Lab07 {
 
         // Add elements
         set.add("APPLE");
-        set.add("B");
-        set.add("C");
-        Object[] arr = set.toArray();
+        set.add("APPLE");
+        set.add("TON");
 
         // Print size
-        System.out.println("Size: " + set.size()); 
+        System.out.println("Size: " + set.size());
 
         // Check if an element is present
         System.out.println("Contains APPLE: " + set.contains("APPLE"));
-        System.out.println("Contains D: " + set.contains("D")); 
+        System.out.println("Contains Tiger: " + set.contains("Tiger"));
 
         // Remove an element
-        System.out.println("Remove D: " + set.remove("D"));
+        System.out.println("Remove TON: " + set.remove("TON"));
 
         // Print size after removal
-        System.out.println("Size: " + set.size()); 
-        System.out.println("to Array: " +Arrays.toString(arr));
+        System.out.println("Size: " + set.size());
+
+        // New collection
+        List<String> arr = Arrays.asList("APPLE", "ORANGE", "BANANA");
+
+        // Check containsAll
+        System.out.println("ContainsAll arr: " + set.containsAll(arr));
+
+        // AddAll
+        System.out.println("AddAll arr: " + set.addAll(arr));
+
+        // Print size after AddAll
+        System.out.println("Size: " + set.size());
+
+        // RetainAll
+        System.out.println("RetainAll arr: " + set.retainAll(Arrays.asList("APPLE")));
+
+        // Print size after RetainAll
+        System.out.println("Size: " + set.size());
+
+        // RemoveAll
+        System.out.println("RemoveAll arr: " + set.removeAll(arr));
     }
 }
